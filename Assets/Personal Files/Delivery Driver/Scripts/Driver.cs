70d3a9714f24ea0908200dc272ecae57d78242ff
Joseph Bermudez <josephbermudez1337@gmail.com>
2023-08-16 01:14:13 -0600

    Add Time variable for speed

--- Assets/Personal Files/Delivery Driver/Scripts/Driver.cs
+++ Assets/Personal Files/Delivery Driver/Scripts/Driver.cs
@@ -8,13 +8,22 @@ namespace Driver
     {
         [SerializeField] private float rotateSpeed = 0.1f;
         [SerializeField] private float moveSpeed = 0.01f;
-        void Update()
+
+        private void Update()
+        {
+            MovePlayer();
+        }
+
+        private void MovePlayer()
         {
             //Rotate the player in Z to change direction
-            transform.Rotate(0, 0, rotateSpeed);
+            float steerAmount = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+            transform.Rotate(0, 0, -steerAmount);
 
-            //Move the player forward in the Y axis.
-            transform.Translate(0, moveSpeed, 0);
+            //Move the player forward in the Y axis
+            float speedAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+            transform.Translate(0, speedAmount, 0);
+
         }
     }
 }
